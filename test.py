1a5cc181a66eaa05b7c5ae5cc54a678d8ce9e351
Jesus Diaz Vico <jesus.diaz.vico@gmail.com>
2023-05-03 00:10:37 +0200

    DID resolution working up to (not included) service endpoint.

--- test.py
+++ test.py
@@ -21,44 +21,28 @@ from peerdid.dids import (
     create_peer_did_numalgo_0,
     create_peer_did_numalgo_2,
     resolve_peer_did,
-    is_peer_did
+    is_peer_did,
 )
 
 from didcomm.secrets.secrets_resolver_in_memory import SecretsResolverInMemory
+from didcomm.secrets.secrets_resolver_demo import SecretsResolverDemo
+from didcomm.secrets.secrets_util import (
+    jwk_to_secret,
+    generate_ed25519_keys_as_jwk_dict,
+    generate_x25519_keys_as_jwk_dict    
+)
 from didcomm.common.resolvers import ResolversConfig
-from didcomm.secrets.secrets_util import jwk_to_secret
 
-from peerdid.keys import Ed25519VerificationKey, X25519KeyAgreementKey
+from peerdid.keys import (
+    Ed25519VerificationKey,
+    X25519KeyAgreementKey,
+    BaseKey,
+    to_multibase
+)
 
 from pydid.doc import DIDDocument
 
 loop = asyncio.get_event_loop()
-
-VALID_X25519_KEY_AGREEMENT_KEY_1 = X25519KeyAgreementKey.from_base58(
-    "JhNWeSVLMYccCk7iopQW4guaSJTojqpMEELgSLhKwRr"    
-)
-
-VALID_X25519_KEY_AGREEMENT_KEY_2 = X25519KeyAgreementKey.from_multibase(
-    "z6LSbysY2xFMRpGMhb7tFTLMpeuPRaqaWM1yECx2AtzE3KCc"
-)
-
-VALID_X25519_KEY_AGREEMENT_KEY_JWK_1 = X25519KeyAgreementKey.from_jwk(
-    {
-        "kty": "OKP",
-        "crv": "X25519",
-        "x": "BIiFcQEn3dfvB2pjlhOQQour6jXy9d5s2FKEJNTOJik",
-    }
-)
-
-VALID_ED25519_VERIFICATION_KEY_1 = Ed25519VerificationKey.from_base58(
-    "ByHnpUCFb1vAfh9CFZ8ZkmUZguURW8nSw889hy6rD8L7"
-)
-
-VALID_ED25519_VERIFICATION_KEY_2 = Ed25519VerificationKey.from_base58(
-    "3M5RCDjPTWPkKSN3sxUmmMqHbmRPegYP1tjcKyrDbt9J"
-)
-
-#from tests.test_vectors.common import ALICE_DID, BOB_DID, CHARLIE_DID
 
 # DID Peer imports
 import json
@@ -67,30 +51,45 @@ from typing import Optional
 from didcomm.common.types import DID, VerificationMethodType, VerificationMaterial, VerificationMaterialFormat
 from didcomm.did_doc.did_doc import DIDDoc, VerificationMethod, DIDCommService
 from didcomm.did_doc.did_resolver import DIDResolver
-#from peerdid import peer_did
-#from peerdid.core.did_doc_types import DIDCommServicePeerDID
-#from peerdid.did_doc import DIDDocPeerDID
-#from peerdid.types import VerificationMaterialFormatPeerDID
 
 ##################
+
+#
+def update_secrets_kids(in_kaKeys, in_sKeys, peerdid):
+    
+    chunks = peerdid[13:].split('.')
+    ctr = 0
+    out_kaKeys = []
+    for jk in in_kaKeys:
+        k = json.loads(jk)
+        k['kid'] = "#"+chunks[ctr][:8:]
+        ctr+=1
+        out_kaKeys.append(json.dumps(k))
+        
+    out_sKeys = []
+    for jk in in_sKeys:
+        k = json.loads(jk)
+        k['kid'] = "#"+chunks[ctr][:8:]
+        ctr+=1
+        out_sKeys.append(json.dumps(k))
+
+    return [out_kaKeys, out_sKeys]
 
 async def create_peerdid(nka, ns):
 
     kaKeys = []
-    sk_kaKeys = []
+    in_kaKeys = []
     for i in range(nka):
-        key = jwk.JWK.generate(kty='OKP', crv='X25519')
-        sk_kaKeys.append(key.export())
-        kaKeys.append(X25519KeyAgreementKey.from_jwk(key.export(private_key=False)))
-        #VALID_X25519_KEY_AGREEMENT_KEY_JWK_1)
+        key = generate_x25519_keys_as_jwk_dict()[0]
+        in_kaKeys.append(json.dumps(key))
+        kaKeys.append(X25519KeyAgreementKey.from_jwk(key))
         
     sKeys = []
-    sk_sKeys = []
+    in_sKeys = []
     for i in range(ns):
-        key = jwk.JWK.generate(kty='OKP', crv='Ed25519')
-        sk_sKeys.append(key.export())
-        sKeys.append(Ed25519VerificationKey.from_jwk(key.export(private_key=False)))
-        #VALID_ED25519_VERIFICATION_KEY_1)
+        key = generate_ed25519_keys_as_jwk_dict()[0]
+        in_sKeys.append(json.dumps(key))
+        sKeys.append(Ed25519VerificationKey.from_jwk(key))
 
     service = {
         "type": "DIDCommMessaging",
@@ -104,6 +103,10 @@ async def create_peerdid(nka, ns):
         signing_keys=sKeys,
         service=service
     )
+
+    # This is a very dirty way to update the 'kid' of the Secrets.
+    # There must be a better way
+    [sk_kaKeys, sk_sKeys] = update_secrets_kids(in_kaKeys, in_sKeys, peer_did_algo_2)
     
     assert is_peer_did(peer_did_algo_2)
     return {
@@ -151,36 +154,43 @@ class DIDResolverPeerDID(DIDResolver):
 
     async def resolve(self, did: DID) -> Optional[DIDDoc]:
         did_doc = resolve_peer_did(peer_did=did)
-        print (json.dumps(did_doc.to_json(), indent=2))
-        return did_doc
+        return DIDDoc(
+            id=did_doc.id,
+            authentication=did_doc.authentication,
+            key_agreement=did_doc.key_agreement,
+            verification_method=did_doc.verification_method,
+#            service=DIDCommService(did_doc.service)
+        )
 
 class MockSecretsResolver(SecretsResolverInMemory):
     def __init__(self, secretsList):
         super().__init__(
             secrets=list(mock_secrets_resolver_alice.MockSecretsResolverAlice()._secrets.values()))
 
-def extend_secrets_list(sl, _sl):
-    for s in _sl:
-        print(jwk.thumbprint_uri(s))
-        sl.append(jwk_to_secret(s))
-
 def main():
+
+    # Create sample DIDs
     peerdid_1ka_0s = loop.run_until_complete(create_peerdid(1,0))
     peerdid_2ka_0s = loop.run_until_complete(create_peerdid(2,0))
-    peerdid_1ka_1s = loop.run_until_complete(create_peerdid(1,1))
-    print(resolve_peer_did(peer_did=peerdid_1ka_0s['peerdid']))
-    secretsList = []
-    extend_secrets_list(secretsList, peerdid_1ka_0s['sk_kaKeys'])
-    extend_secrets_list(secretsList, peerdid_1ka_0s['sk_sKeys'])
-    extend_secrets_list(secretsList, peerdid_2ka_0s['sk_kaKeys'])
-    extend_secrets_list(secretsList, peerdid_2ka_0s['sk_sKeys'])
-    extend_secrets_list(secretsList, peerdid_1ka_1s['sk_kaKeys'])
-    extend_secrets_list(secretsList, peerdid_1ka_1s['sk_sKeys'])
-    print(secretsList)
-    
-    secretsResolver = MockSecretResolver(secretsList)
-    
-    resolvers_config = ResolversConfig(mock_secrets_resolver_alice.MockSecretsResolverAlice, DIDResolverPeerDID())
+    peerdid_1ka_1s = loop.run_until_complete(create_peerdid(1,1))    
+
+    # Create custom resolver
+    secrets_resolver = SecretsResolverDemo()
+    for s in peerdid_1ka_0s['sk_kaKeys']:
+        loop.run_until_complete(secrets_resolver.add_key(jwk_to_secret(json.loads(s))))
+    for s in peerdid_1ka_0s['sk_sKeys']:
+        loop.run_until_complete(secrets_resolver.add_key(jwk_to_secret(json.loads(s))))
+    for s in peerdid_2ka_0s['sk_kaKeys']:
+        loop.run_until_complete(secrets_resolver.add_key(jwk_to_secret(json.loads(s))))
+    for s in peerdid_2ka_0s['sk_sKeys']:
+        loop.run_until_complete(secrets_resolver.add_key(jwk_to_secret(json.loads(s))))
+    for s in peerdid_1ka_1s['sk_kaKeys']:
+        loop.run_until_complete(secrets_resolver.add_key(jwk_to_secret(json.loads(s))))
+    for s in peerdid_1ka_0s['sk_sKeys']:
+        loop.run_until_complete(secrets_resolver.add_key(jwk_to_secret(json.loads(s))))
+    resolvers_config = ResolversConfig(secrets_resolver, DIDResolverPeerDID())
+
+    # Authcrypt packaging
     loop.run_until_complete(pack_authcrypt(peerdid_1ka_0s['peerdid'], resolvers_config,
                                            peerdid_2ka_0s['peerdid'], resolvers_config,
                                            peerdid_1ka_1s['peerdid'], resolvers_config))
