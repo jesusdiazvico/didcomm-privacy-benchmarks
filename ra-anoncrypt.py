aa7f49d9fbe257642f7e35001bd3e44d3afb1593
Jesus Diaz Vico <jesus.diaz.vico@gmail.com>
2023-05-17 22:53:01 +0200

    Receiver anonymous anoncrypt working.

--- ra-anoncrypt.py
+++ ra-anoncrypt.py
@@ -5,24 +5,34 @@ import time
 import numpy
 import json
 import secrets
+import random
 
 import hashlib
 
 from typing import List
 from copy import deepcopy
+from collections import OrderedDict
 
 from authlib.jose import JsonWebEncryption
-from authlib.jose.rfc8037 import OKPKey
+from authlib.jose.rfc7516.models import JWEHeader
 from authlib.jose.rfc7517 import AsymmetricKey
 from authlib.common.encoding import (
     to_unicode,
     to_bytes,
     urlsafe_b64encode,
+    urlsafe_b64decode,
     json_dumps,
     json_loads,
     json_b64encode,
 )
 from authlib.jose.rfc7518 import ECDHESAlgorithm, CBCHS2EncAlgorithm
+from authlib.jose.util import (
+    ensure_dict,
+    extract_header,
+    extract_segment
+)
+from authlib.jose.rfc8037 import OKPKey
+from authlib.jose.errors import DecodeError
 
 from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHash
 from cryptography.hazmat.primitives.ciphers.aead import AESCCM
@@ -90,9 +100,11 @@ def raanoncrypt(msg, pks):
 
     # Compute ephemeral key pair for flagging
     ekp = gen_keys(1)[0]
-    _apv = [ str(ekp.as_dict(is_private=False)) ] # @TODO Check for better ways to encode this
+    _apv = {}    
+    _apv['ekp'] = ekp.as_dict(is_private=False)
     
     # Encrypt flag per recipient
+    flags = []
     for pk in pks:
 
         # Run ECDH
@@ -101,9 +113,9 @@ def raanoncrypt(msg, pks):
         # Run KDF to get flag encryption key
         ckdf = ConcatKDFHash(
             algorithm = hashes.SHA256(),
-            length=32,
-            otherinfo=bytes("ra-anon", "utf-8"),
-            backend=default_backend()
+            length = 32,
+            otherinfo = bytes("ra-anon", "utf-8"),
+            backend = default_backend()
         )
         fk = ckdf.derive(shk)
 
@@ -114,16 +126,16 @@ def raanoncrypt(msg, pks):
         c = aes.encrypt(nonce=nonce,
                         data=b'\x01',
                         associated_data=aad)
-        _apv.append(str(c)) # @TODO Check for better ways to encode this
+        flags.append([
+            urlsafe_b64encode(c).decode('utf-8'),
+            urlsafe_b64encode(nonce).decode('utf-8'),
+        ])
 
     # Base64-encode the _apv array
-    apv = to_unicode(
-        urlsafe_b64encode(bytes(".".join(sorted(_apv)),'utf-8'))
-    )    
+    _apv['flags'] = flags
+    apv = to_unicode(urlsafe_b64encode(json.dumps(_apv).encode('utf-8')))
         
     header = build_header(pks, apv)
-
- #    jwe = JsonWebEncryption()
 
     ## From here on, we essentially copy the serialize_json function in
     ## authlib.jose.rfc7516.jwe, but reusing the ekp computed above, and
@@ -139,15 +151,15 @@ def raanoncrypt(msg, pks):
         if 'header' not in recipients[i]:
             recipients[i]['header'] = {}
 
-    jwe_aad = header_obj.get('aad') # @TODO: we don't pass an aad...
+    jwe_aad = header_obj.get('aad')
     
     if len(pks) != len(recipients):
         raise ValueError("Count of recipient keys {} does not equal to count of recipients{}"
-                         .format(len(pks), len(recipients))) # @TOIMPORT ValueError
+                         .format(len(pks), len(recipients)))
 
 
     # Step 1: Prepare algorithms and key
-    alg = ECDHESAlgorithm()
+    alg = ECDHESAlgorithm(256)
     enc = CBCHS2EncAlgorithm(256, 512)
 
     for i in range(len(pks)):
@@ -155,45 +167,27 @@ def raanoncrypt(msg, pks):
 
     # Step 3: Encrypt the CEK
     preset = alg.generate_preset(enc, pks[0])
+
     if 'cek' in preset:
         cek = preset['cek']
     else:
         cek = None
 
-    if len(pks) > 1 and cek is None:
-        raise InvalidAlgorithmForMultipleRecipientsMode(alg.name) # @TOIMPORT
+#    if len(pks) > 1 and cek is None:
+#        raise ValueError("InvalidAlgorithmForMultipleRecipientsMode {}"
+#                         .format(alg.name))
     if 'header' in preset:
         shared_header.update_protected(preset['header'])
 
-    # This is a tag-aware key agreement, so delaying CEK encryption...
-    epks = []
-    print("Remember to check that the previously computed ekp is used here.")
+    # This is not a tag-aware key agreement, so not delaying CEK encryption...
     for i in range(len(pks)):
-
-        # prep = enc.generate_keys_and_prepare_headers(pks[i], None, preset)
-        if preset and 'epk' in preset:
-            epk = preset['epk']
-            h = {}
-        else:
-            epk = pks[i].generate_key(key['crv'], is_private=True)
-            pub_epk = {k: epk[k] for k in epk.REQUIRED_JSON_FIELDS}
-            pub_epk['kty'] = epk.kty
-            h = {'epk': pub_epk }
-
-        if preset and 'cek' in preset:
-            cek = preset['cek']
-        else:
-            cek = enc.generate_cek()
-
-        prep = { 'epk': epk, 'cek': cek, 'header': h }
-        
+        wrapped = alg.wrap(enc, shared_header, pks[i], preset)
         if cek is None:
-            cek = prep['cek']
-            
-        epks.append(prep['epk'])
-        
-        recipients[i]['header'].update(prep['header'])
-
+            cek = wrapped['cek']        
+        recipients[i]['encrypted_key'] = wrapped['ek']
+        if 'header' in wrapped:
+            recipients[i]['header'].update(wrapped['header'])
+    
     # Step 4: Generate a random JWE Initialization Vector
     iv = enc.generate_iv()
 
@@ -204,34 +198,22 @@ def raanoncrypt(msg, pks):
     aad = to_bytes(aad, 'ascii')
 
     # Step 6: No compression
-    print(msg)
     _msg = encode(msg)
 
     # Step 7: Perform encryption
     ciphertext, tag = enc.encrypt(_msg, aad, iv, cek)
 
-    # Run the delayed CEK encryption
-    for i in range(len(pks)):
-        wrapped = enc.agree_upon_key_and_wrap_cek(shared_header,
-                                                  pks[i],
-                                                  None,
-                                                  epks[i],
-                                                  cek,
-                                                  tag)
-        recipients[i]['encrypted_key'] = wrapped['ek']
-
     # Step 8: Build resulting message
     obj = OrderedDict()
 
     if shared_header.protected:
-        obj['protected'] = to_unicode(json-b64encode(shared_header.protected))
+        obj['protected'] = to_unicode(json_b64encode(shared_header.protected))
 
     if shared_header.unprotected:
         obj['unprotected'] = shared_header.unprotected
 
     for r in recipients:
-        if not r['header']:
-            del r['header']
+        del r['header'] # Anonymous receivers, we don't want kids
         r['encrypted_key'] = to_unicode(urlsafe_b64encode(r['encrypted_key']))
         for member in set(r.keys()):
             if member not in {'header', 'encrypted_key'}:
@@ -248,11 +230,110 @@ def raanoncrypt(msg, pks):
     return obj
 
 def raanondecrypt(ctxt, sks):
-    header = build_header(sks)
-    jwe = JsonWebEncryption()
-    # For testing purposes, we only decrypt with the first recpient's key
-    dec = decode(jwe.deserialize_json(ctxt,sks[0])['payload'])
-    return dec
+
+    # First, parse header as in deserialize_json@authlib.jose.rfc7516.jwe.py
+    ctxt = ensure_dict(ctxt, 'JWE')
+    ctxt = deepcopy(ctxt)
+
+    if 'protected' in ctxt:
+        protected = extract_header(to_bytes(ctxt['protected']), DecodeError)
+    else:
+        protected = None
+
+    unprotected = ctxt.get('unprotected')
+
+    recipients = ctxt['recipients']
+    for r in recipients:
+        if 'header' not in r:
+            r['header'] = {}
+        r['encrypted_key'] = extract_segment(
+            to_bytes(r['encrypted_key']), DecodeError, 'encrypted key'
+        )
+
+    if 'aad' in ctxt:
+        jwe_aad = extract_segment(to_bytes(ctxt['aad']), DecodeError, 'JWE AAD')
+    else:
+        jwe_aad = None
+
+    iv = extract_segment(
+        to_bytes(ctxt['iv']), DecodeError, 'iv')    
+    ciphertext = extract_segment(to_bytes(ctxt['ciphertext']), DecodeError, 'ciphertext')
+    tag = extract_segment(to_bytes(ctxt['tag']), DecodeError, 'tag')
+    shared_header = JWESharedHeader(protected, unprotected)
+
+    alg = ECDHESAlgorithm(256)
+    enc = CBCHS2EncAlgorithm(256, 512)
+    key = alg.prepare_key(sks[0])
+    kid = "did:example:bob#key0" # In our PoC, we always decrypt with Bob's first key
+    
+    # Parse apv for potential matches
+    apv = extract_segment(to_bytes(protected['apv']), DecodeError, 'apv').decode('utf-8')
+
+    ## Retrieve the ekp
+    ekp = OKPKey.import_dict_key(json.loads(apv)['ekp'])
+
+    ## Trial-decrypt the flags
+    flags = json.loads(apv)['flags']
+
+    # For simulation purposes, pick "randomly" one key within the array
+    # this makes the trial decryption more realistic
+    rnd = random.randint(0,len(sks)-1)
+    key = sks[rnd]
+    match = False
+    i = 0
+    index = 0
+    for fn in flags:
+        flag = fn[0]
+        nonce = fn[1]
+        shk = key.exchange_shared_key(ekp.get_public_key())
+        ckdf = ConcatKDFHash(
+            algorithm = hashes.SHA256(),
+            length = 32,
+            otherinfo = bytes("ra-anon", "utf-8"),
+            backend = default_backend()
+        )
+        fk = ckdf.derive(shk)
+        aes = AESCCM(fk)
+        f = aes.decrypt(nonce=urlsafe_b64decode(bytes(nonce, "utf-8")),
+                        data=urlsafe_b64decode(bytes(flag, "utf-8")),
+                        associated_data=bytes("ra-anon", "utf-8"))
+        if f == b'\x01':
+            match = True
+            index = i
+            break
+
+        i = i+1
+
+    if match == False:
+        return None    
+    
+    # If we reach this point, there was a match in the encrypted flags, so
+    # go get the cek
+    ek = ctxt['recipients'][index]['encrypted_key']
+    header = JWEHeader(protected, unprotected, {})
+    cek = alg.unwrap(enc, ek, header, key)
+
+
+    # Decrypt the actual payload
+    aad = to_bytes(ctxt.get('protected', ''))
+    if 'aad' in ctxt:
+        aad += b'.' + to_bytes(obj['aad'])
+    aad = to_bytes(aad, 'ascii')
+    
+    dec = enc.decrypt(ciphertext, aad, iv, tag, cek)
+
+    h = {}
+    if protected:
+        h['protected'] = protected
+    if unprotected:
+        h['unprotected'] = unprotected
+    if jwe_aad is not None:
+        header['aad'] = jwe_aad
+
+    return {
+        'header': h,
+        'payload': decode(dec)
+    }
 
 def main():
 
@@ -281,7 +362,10 @@ def main():
 
         # Measure time for "anondecrypting"
         st_decrypt = time.process_time()
-#        dec_json = anondecrypt(ctxt, recipients)
+        dec_json = raanondecrypt(ctxt, recipients)
+        if dec_json == None:
+            print("Unexpected mismatch.")
+            return
         et_decrypt = time.process_time()
         anondecrypt_times.append(et_decrypt - st_decrypt)
 
