7cc0f95545ce9f92f9577750bb424070c7282666
Jesus Diaz Vico <jesus.diaz.vico@gmail.com>
2023-05-25 11:43:01 +0200

    Renamed variables in flask.

--- flask-app/application.py
+++ flask-app/application.py
@@ -9,7 +9,7 @@ app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 @app.route('/')
 @app.route('/index')
 def show_index():
-    full_filename_enc = os.path.join(app.config['UPLOAD_FOLDER'], 'results-enc-cpu.png')
-    full_filename_dec = os.path.join(app.config['UPLOAD_FOLDER'], 'results-dec-cpu.png')
+    full_filename_enc = os.path.join(app.config['UPLOAD_FOLDER'], 'results-enc.png')
+    full_filename_dec = os.path.join(app.config['UPLOAD_FOLDER'], 'results-dec.png')
     full_filename_size = os.path.join(app.config['UPLOAD_FOLDER'], 'results-size.png')
-    return render_template("index.html", enc_cpu_image = full_filename_enc, dec_cpu_image = full_filename_dec, size_image = full_filename_size)
+    return render_template("index.html", enc_image = full_filename_enc, dec_image = full_filename_dec, size_image = full_filename_size)
